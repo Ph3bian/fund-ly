a408c83e0946113aebb14ceea418b25cc6989c01
Phebian Chukwurah <phebian@softcom.ng>
2018-12-11 04:01:21 +0200

    deploy on heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+//Install express server
+const express = require('express');
+// const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', (req, res) => {
+	res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(process.env.PORT || 7004);
